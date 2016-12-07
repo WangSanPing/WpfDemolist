e16983903a582ea03f51b5c443b12871cf114025
WangSanPing <wangxu_1987@126.com>
2016-12-07 17:41:09 +0800

    测试git

--- WpfDemolist/DemoControlTemplate.xaml.cs
+++ WpfDemolist/DemoControlTemplate.xaml.cs
@@ -10,30 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfDemolist
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// DemoControlTemplate.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class DemoControlTemplate : Window
     {
-        public MainWindow()
+        public DemoControlTemplate()
         {
             InitializeComponent();
-        }
-
-        /// <summary>
-        /// 控件模板
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            DemoControlTemplate window = new DemoControlTemplate();
-            window.Show();
         }
     }
 }
