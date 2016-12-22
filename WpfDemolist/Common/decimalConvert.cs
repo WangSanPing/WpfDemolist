df3d78f5bec6e123bafe492f96abe3e507d50c72
WangSanPing <wangxu_1987@126.com>
2016-12-22 14:50:10 +0800

    修改了dataformatdemo

---
+++ WpfDemolist/Common/decimalConvert.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfDemolist.Common
+{
+    public class decimalConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string currencyText = "";
+            if (value != null)
+            {
+                currencyText = ((decimal)value).ToString("C");
+            }
+
+            return currencyText;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string price = ((decimal)value).ToString(culture);
+
+            decimal result;
+            if (Decimal.TryParse(price, NumberStyles.Any, culture, out result))
+            {
+                return result;
+            }
+
+            return value;
+        }
+    }
+}
