b3e5712b0ff314d9a24db37868822e9c096a8e68
WangSanPing <wangxu_1987@126.com>
2016-12-28 17:54:58 +0800

    自定义ListView未完成

---
+++ WpfDemolist/DataView/TitleView.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfDemolist.DataView
+{
+    public class TileView : ViewBase
+    {
+        private DataTemplate itemTemplate;
+
+        public DataTemplate ItemTemplate
+        {
+            get
+            {
+                return itemTemplate;
+            }
+
+            set
+            {
+                itemTemplate = value;
+            }
+        }
+
+        protected override object DefaultStyleKey
+        {
+            get { return new ComponentResourceKey(GetType(), "TileView"); }
+        }
+
+        protected override object ItemContainerDefaultStyleKey
+        {
+            get { return new ComponentResourceKey(GetType(), "TileViewItem"); }
+        }
+    }
+}
